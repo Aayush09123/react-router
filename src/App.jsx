21074b8d7752d6e2734d52f41cc62e99a314471a
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-03-12 09:52:46 +0530

    add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import "./App.css"
 import Home from "./components/Home/Home";
 import { useRoutes } from "react-router-dom";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/about", element: <About /> },
+    { path: "/contact", element: <Contact /> },
   ])
   return routes
 }
